7d62360be00e52b10ecaf4001e1179230785da52
tthwy <i23xiaoyan@qq.com>
2019-09-05 15:34:26 +0800

    v1.0.8

--- library/src/main/java/com/gallopmark/imagepicker/BaseActivity.java
+++ library/src/main/java/com/gallopmark/imagepicker/BaseActivity.java
@@ -1,21 +1,24 @@
 package com.gallopmark.imagepicker;
 
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Window;
+
 
 abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.ImagePickerTheme);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(bindContentView());
         setupToolbar();
         setup(savedInstanceState);
@@ -23,21 +26,16 @@ abstract class BaseActivity extends AppCompatActivity {
 
     private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolBar);
-        if (toolbar != null) {
-            toolbar.setTitle("");
-            Drawable navigationIcon = ContextCompat.getDrawable(this, R.drawable.icon_back);
-            if (navigationIcon != null) {
-                navigationIcon.setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_ATOP);
-            }
-            toolbar.setNavigationIcon(navigationIcon);
-            setSupportActionBar(toolbar);
-            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    onBackPressed();
-                }
-            });
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 
     protected abstract int bindContentView();
